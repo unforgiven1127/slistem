61e4f3231ea1805763d6836dcbdee1a2727067c6
Dominykas Cepulis <dcepulis@slate-ghc.com>
2015-10-25 17:42:45 -0700

    Updated PHPMailer.

--- component/mail/phpmailer/language/phpmailer.lang-he.php
+++ component/mail/phpmailer/language/phpmailer.lang-he.php
@@ -1,15 +1,15 @@
 <?php
 /**
-* PHPMailer language file: refer to English translation for definitive list
-* Hebrew Version, UTF-8
-* by : Ronny Sherer <ronny@hoojima.com>
-*/
+ * Hebrew PHPMailer language file: refer to English translation for definitive list
+ * @package PHPMailer
+ * @author Ronny Sherer <ronny@hoojima.com>
+ */
 
 $PHPMAILER_LANG['authenticate']         = 'שגיאת SMTP: פעולת האימות נכשלה.';
 $PHPMAILER_LANG['connect_host']         = 'שגיאת SMTP: לא הצלחתי להתחבר לשרת SMTP.';
 $PHPMAILER_LANG['data_not_accepted']    = 'שגיאת SMTP: מידע לא התקבל.';
 $PHPMAILER_LANG['empty_message']        = 'גוף ההודעה ריק';
-$PHPMAILER_LANG['invalid_address']      = 'כתובת שגויה';
+$PHPMAILER_LANG['invalid_address']      = 'כתובת שגויה: ';
 $PHPMAILER_LANG['encoding']             = 'קידוד לא מוכר: ';
 $PHPMAILER_LANG['execute']              = 'לא הצלחתי להפעיל את: ';
 $PHPMAILER_LANG['file_access']          = 'לא ניתן לגשת לקובץ: ';
